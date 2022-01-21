4e19f3dfdc0beb172b634686e28d8f6737f25160
Marc <marcjacques@me.com>
2022-01-21 11:56:29 -0500

    Add styling to notification list

--- Instagram/Views/NotificationsView.swift
+++ Instagram/Views/NotificationsView.swift
@@ -12,7 +12,9 @@ struct NotificationsView: View {
         NavigationView {
             VStack {
                 List(0...30, id: \.self) { num in
-                    NotificationFollowView()
+                    if num % 2 == 0 {
+                        NotificationFollowView()
+                    }
                 }
             }
             .navigationTitle("Notifications")
@@ -24,15 +26,29 @@ struct NotificationFollowView: View {
     var body: some View {
         HStack {
             //user image
-            Image("user1")
+            Image("user\(Int.random(in: 1...4))")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 40, height: 40, alignment: .center)
                 .cornerRadius(20)
             
             //text, and date
-            
+            VStack(alignment: .leading) {
+                Text("ThisGuy started following you.")
+                Text("\(Int.random(in: 3...59)) minutes ago")
+                    .foregroundColor(Color(.secondaryLabel))
+            }
+            Spacer()
             // follow button
+            Button(action: {
+                //do nothing
+            }, label: {
+                Text("Follow")
+                    .frame(width: 80, height: 30, alignment: .center)
+                    .foregroundColor(.white)
+                    .background(Color(.link))
+                    .cornerRadius(8)
+            })
         }
     }
 }
