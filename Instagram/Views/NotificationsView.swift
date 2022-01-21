4e332e0ac3e68ecd8960de824f9cd67001145e57
Marc <marcjacques@me.com>
2022-01-21 11:48:15 -0500

    Add List for notifications

--- Instagram/Views/NotificationsView.swift
+++ Instagram/Views/NotificationsView.swift
@@ -9,12 +9,37 @@ import SwiftUI
 
 struct NotificationsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                List(0...30, id: \.self) { num in
+                    NotificationFollowView()
+                }
+            }
+            .navigationTitle("Notifications")
+        }
+    }
+}
+
+struct NotificationFollowView: View {
+    var body: some View {
+        HStack {
+            //user image
+            Image("user1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 40, height: 40, alignment: .center)
+                .cornerRadius(20)
+            
+            //text, and date
+            
+            // follow button
+        }
     }
 }
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
         NotificationsView()
+            .preferredColorScheme(.dark)
     }
 }
