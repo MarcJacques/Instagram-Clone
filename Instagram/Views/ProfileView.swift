bbd6c0978c0560737b44e97e08006fd2abca573b
Marc <marcjacques@me.com>
2022-01-22 18:56:58 -0500

    Add Profile Header setup

--- Instagram/Views/ProfileView.swift
+++ Instagram/Views/ProfileView.swift
@@ -9,12 +9,34 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                
+            }
+            .navigationBarTitle("instaClone", displayMode: .inline)
+        }
+    }
+}
+
+struct ProfileHeaderView: View  {
+    var body: some View {
+        VStack {
+            // user avatar & following counts
+            HStack {
+                Image("Image \(Int.random(in: 1...4))")
+                    .resizable()
+                    .aspectRatio( contentMode: .fill)
+                    .frame(width: 40, height: 40, alignment: .center)
+            }
+            //edit profile button
+            //user bio
+        }
     }
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
+            .preferredColorScheme(.dark)
     }
 }
